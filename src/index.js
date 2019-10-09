60122e0538dea42d22aabb2e974b6cb923abebf9
joaquinto <roejoeodj12@gmail.com>
2019-10-09 10:45:23 +0100

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import http from 'http';
 import log from 'fancy-log';
-import cluster from 'cluster';
-import { cpus } from 'os';
 import app from './app';
 import socketConnection from './helpers/socketIo';
 import constants from './config/constants';
 import client from './helpers/redis';
 
 const { PORT } = constants;
-const noOfCpu = cpus().length;
 
-if (cluster.isMaster) {
-  log(`master ${process.pid} is running`);
-  client.flushall((error) => {
-    if (error) throw new Error(error);
-    log('Data cache has been cleared');
-  });
-  // fork workers
-  for (let i = 0; i < noOfCpu; i++) {
-    cluster.fork();
-  }
+const server = http.createServer(app);
 
-  cluster.on('exit', (worker, code, signal) => {
-    log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  const server = http.createServer(app);
+client.flushall((error) => {
+  if (error) throw new Error(error);
+  log('Data cache has been cleared');
+});
 
-  socketConnection(server);
+socketConnection(server);
 
-  server.listen(PORT, () => {
-    log(`server running on port ${PORT}`);
-  });
-
-  log(`worker ${process.pid} started`);
-}
+server.listen(PORT, () => {
+  log(`server running on port ${PORT}`);
+});
